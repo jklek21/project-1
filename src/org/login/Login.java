da88574bd97566699a42603aac53205c2cb42b26
unknown <user.jklek21>
2021-08-13 11:14:17 +0530

    Login Credential

---
+++ src/org/login/Login.java
@@ -0,0 +1,18 @@
+package org.login;
+
+public class Login {
+
+	public void userNam() {
+		System.out.println("UserName");
+   }
+	
+	public void PassWord() {
+		System.out.println("Pass");
+}
+	public static void main(String[] args) {
+		Login l=new Login();
+		l.userNam();l.PassWord();
+		
+	}
+}
+
